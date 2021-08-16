1049af6cec0d85fce21ad96bcdf13e57c14c36f2
romanogiordani <romano.giordani@gmail.com>
2021-08-16 10:36:33 -0300

    Update description
    Script para impressão de etiquetas via impressora de gondola

--- etq.bat
+++ etq.bat
@@ -3,7 +3,7 @@ IF EXIST z:\final.txt ( GOTO :IMPRIME ) ELSE ( GOTO :ESPERA )
 
 :IMPRIME
 del z:\final.txt
-copy z:\gond.txt \\192.168.10.138\etqvisconde
+copy z:\gond.txt \\192.168.10.138\etqviscondehttps://drive.google.com/drive/folders/13BEn99jxah09kLYaNuVUpl-Mfdf2fYgB
 del Z:\gond.txt
 goto :LOOP
 

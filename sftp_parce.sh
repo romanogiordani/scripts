ae40ae226e8a9e5b2f22de9f409b5b4ff12b8e01
romanogiordani <romano.giordani@gmail.com>
2021-08-20 12:13:03 -0300

    Update sftp_parce.sh

--- sftp_parce.sh
+++ sftp_parce.sh
@@ -1,5 +1,4 @@
-export SSHPASS="A.info1991"
-sshpass -p 'A.info1991' sftp root@104.248.239.22:/home/cli_ftp/backups_clientes/stangerlin!
+sshpass -p 'password_aqui' sftp root@104.248.239.22:/home/cli_ftp/backups_clientes/stangerlin!
    cd /backup/mysql/
    put sgdados.tar
    bye
